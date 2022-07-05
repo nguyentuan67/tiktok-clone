c991de9a2f5a4f6bf3e43efe15dc6fb53a857258
nguyentuan <dungtuann67@gmail.com>
2022-07-05 09:23:20 +0700

    tai cau truc va su dung propTypes

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,5 +1,6 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import  Header  from '~/components/Layout/components/Header'
+import  Header  from '~/layouts/components/Header'
 import Sidebar from './Sidebar';
 import styles from './DefaultLayout.module.scss'
 
@@ -17,6 +18,10 @@ function DefaultLayout({ children }) {
             </div>
         </div>
      );
+}
+
+DefaultLayout.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 export default DefaultLayout;
